d2f254f6e9ee3421c0efd52fb0724e5576c22b84
Ponomarev Stepan <htconesuper11@gmail.com>
2020-10-31 17:30:49 +0300

    separated a bit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@
 
   
   while (true != false && count <= amount) {
-    console.log(" ".repeat((amount - count)/2) + "*".repeat(count++) + " ".repeat((amount- count)/2)) ;
+    console.log(
+      " ".repeat((amount - !(count % 2) ? count - 1: count+1)/2),
+      "*".repeat(count++),
+      " ".repeat((amount- !(count % 2) ? count  : count - 2)/2)
+    );
   }
 }
 )()
